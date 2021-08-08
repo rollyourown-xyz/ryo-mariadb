6ba8ca882f6649ee1adb0c1cb536824363aad17c
xyzroller <xyzroller@rollyourown.xyz>
2021-08-08 17:29:32 +0200

    Added build-images script and packer template

---
+++ build-images.sh
@@ -0,0 +1,62 @@
+### TO CHECK: Do we need consul-template?
+
+
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+helpMessage()
+{
+   echo "build-images.sh: Use packer to build images for deployment"
+   echo ""
+   echo "Help: build-images.sh"
+   echo "Usage: ./build-images.sh -n hostname -v version"
+   echo "Flags:"
+   echo -e "-n hostname \t\t\t(Mandatory) Name of the host for which to build images"
+   echo -e "-v version \t\t\t(Mandatory) Version stamp to apply to images, e.g. 20210101-1"
+   echo -e "-m mariadb_version \t(Optional) Override default mariadb version to use for the mariadb image, e.g. 10.6.4 (default)"
+   #echo -e "-c consul_template_version \t(Optional) Override default consul-template version to use for the mariadb image, e.g. 0.26.0 (default)"
+   echo -e "-h \t\t\t\tPrint this help message"
+   echo ""
+   exit 1
+}
+
+errorMessage()
+{
+   echo "Invalid option or mandatory input variable is missing"
+   echo "Use \"./host-setup.sh -h\" for help"
+   exit 1
+}
+
+# Default software versions
+mariadb_version='10.6.4'
+#consul_template_version='0.26.0'
+
+while getopts n:c:v:h flag
+do
+    case "${flag}" in
+        n) hostname=${OPTARG};;
+        v) version=${OPTARG};;
+        m) mariadb_version=${OPTARG};;
+        #c) consul_template_version=${OPTARG};;
+        h) helpMessage ;;
+        ?) errorMessage ;;
+    esac
+done
+
+if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$mariadb_version" ] # || [ -z "$consul_template_version" ]
+then
+   errorMessage
+fi
+
+
+echo "Building images for ryo-coturn module on "$hostname""
+echo ""
+echo "Building Coturn image"
+echo ""
+#echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"version="$version"\" -var \"mariadb_version="$mariadb_version"\" -var \"consul_template_version="$consul_template_version"\" "$SCRIPT_DIR"/image-build/mariadb.pkr.hcl"
+echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"version="$version"\" -var \"mariadb_version="$mariadb_version"\" "$SCRIPT_DIR"/image-build/mariadb.pkr.hcl"
+echo ""
+#packer build -var "host_id="$hostname"" -var "version="$version"" -var "mariadb_version="$mariadb_version"" -var "consul_template_version="$consul_template_version"" "$SCRIPT_DIR"/image-build/mariadb.pkr.hcl
+packer build -var "host_id="$hostname"" -var "version="$version"" -var \"mariadb_version="$mariadb_version"\" "$SCRIPT_DIR"/image-build/mariadb.pkr.hcl
+echo ""
