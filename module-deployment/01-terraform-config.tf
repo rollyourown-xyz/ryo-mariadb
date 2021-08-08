6736e0eab433393fc8c9721d0b6983f105d7ece2
xyzroller <xyzroller@rollyourown.xyz>
2021-08-08 22:08:41 +0200

    Added mariadb module deployment terraform files

---
+++ module-deployment/01-terraform-config.tf
@@ -0,0 +1,34 @@
+# The terraform lxd and consul providers are required to deploy this module
+###########################################################################
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    lxd = {
+      source  = "terraform-lxd/lxd"
+      version = "~> 1.5.0"
+    }
+    # consul = {
+    #   source = "hashicorp/consul"
+    #   version = "~> 2.12.0"
+    # }
+  }
+}
+
+provider "lxd" {
+
+  config_dir                   = "$HOME/snap/lxd/common/config"
+  generate_client_certificates = false
+  accept_remote_certificate    = false
+
+  lxd_remote {
+    name     = var.host_id
+    default  = true
+  }
+}
+
+# provider "consul" {
+#   address    = join("", [ local.consul_ip_address, ":8500" ])
+#   scheme     = "http"
+#   datacenter = var.host_id
+# }
---
+++ module-deployment/02-deploy-mariadb.tf
@@ -0,0 +1,53 @@
+# Deploy MariaDB for rollyourown.xyz projects
+#############################################
+
+### Depoy mariadb container
+resource "lxd_container" "mariadb" {
+
+  remote     = var.host_id
+  name       = join("-", [ var.host_id, local.module_id, "mariadb" ])
+  image      = join("-", [ local.module_id, "mariadb", var.image_version ])
+  profiles   = ["default"]
+  
+  config = { 
+    "security.privileged": "false"
+    "user.user-data" = file("cloud-init/cloud-init-mariadb.yml")
+  }
+  
+  ## Provide eth0 interface with static IP address
+  device {
+    name = "eth0"
+    type = "nic"
+
+    properties = {
+      name           = "eth0"
+      network        = var.host_id
+    }
+  }
+  
+  ## Mount container directory for mariadb data
+  device {
+    name = "mariadb-data"
+    type = "disk"
+    
+    properties = {
+      source   = "/var/containers/ryo-mariadb/data"
+      path     = "/var/lib/mysql"
+      readonly = "false"
+      shift    = "true"
+    }
+  }
+
+  ## Mount container directory for mariadb backups
+  device {
+    name = "mariadb-backups"
+    type = "disk"
+    
+    properties = {
+      source   = "/var/containers/ryo-mariadb/backup"
+      path     = "/var/mdbbackup"
+      readonly = "false"
+      shift    = "true"
+    }
+  }
+}
